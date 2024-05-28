d9544aeaa3500eb73c6960ba8e0bca47b7c59483
etheliachoi <etheliachoi@gmail.com>
2024-05-28 17:41:40 +0800

    Enable calculator functionality when user presses equal sign

--- script.js
+++ script.js
@@ -15,22 +15,78 @@ function divide(a, b) {
 }
 
 function operate(operator, a, b) {
-    if (operator == "+") add(a, b);
-    else if (operator == "-") subtract(a, b);
-    else if (operator == "x") multiply(a, b);
-    else if (operator == "÷") divide(a, b);
+    if (operator == "+") return add(a, b);
+    else if (operator == "-") return subtract(a, b);
+    else if (operator == "x") return multiply(a, b);
+    else if (operator == "÷") return divide(a, b);
 }
 
-let firstNum;
-let secondNum;
-let operator;
+let firstNum = null;
+let secondNum = null;
+let operator = null;
 const displayElem = document.querySelector("#display");
+const resultElem = document.querySelector("#result");
 const digitBtns = document.querySelectorAll(".digit");
-const clear = document.querySelector(".clear");
+const opBtns = document.querySelectorAll(".operator");
+const eqBtn = document.querySelector(".equal");
+const clrBtn = document.querySelector(".clear");
 
-function updateDisplay() {
+function clickDigit(btn) {
+    // operator is null
     digitBtns.forEach(function(btn) {
-        btn.addEventListener("click", () => displayElem.textContent = btn.textContent);
+        btn.addEventListener("click", () => {
+            displayElem.textContent += btn.textContent;
+            if (!operator) {
+                if (!firstNum) firstNum = btn.textContent;
+                else firstNum += btn.textContent;
+            } else {
+                if (!secondNum) secondNum = btn.textContent;
+                else {
+                    secondNum += btn.textContent;
+                }
+            }
+        });
     })
 }
-updateDisplay();
+
+function clickOperator(btn) {
+    opBtns.forEach(function(btn) {
+        btn.addEventListener("click", () => {
+            displayElem.textContent += btn.textContent;
+            operator = btn.textContent;
+            disableOps();
+        })
+    })
+}
+
+function disableOps() {
+    opBtns.forEach((btn) => btn.disabled = true);
+}
+
+function enableOps() {
+    opBtns.forEach((btn) => btn.disabled = false);
+}
+
+clickDigit();
+clickOperator();
+clickEqual();
+clickClear();
+
+function clickEqual() {
+    eqBtn.addEventListener("click", () => {
+        let resultInt = operate(operator, parseInt(firstNum), parseInt(secondNum));
+        resultElem.textContent = resultInt.toString();
+        // firstNum, secondNum = resultInt, null;
+        enableOps();
+    })
+}
+
+function clickClear() {
+    clrBtn.addEventListener("click", () => {
+        firstNum = null;
+        secondNum = null;
+        operator = null;
+        displayElem.textContent = null;
+        enableOps();
+    })
+}
